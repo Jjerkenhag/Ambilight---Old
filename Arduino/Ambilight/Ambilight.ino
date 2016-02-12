46b094dd2b0dd774df53b59ce4384e797a11a8f1
Jjerkenhag <jjerkenhag@gmail.com>
2016-02-12 23:00:23 +0100

    Added comments to Arduino code
    Added descriptive comments to the Arduino code

--- Arduino/Ambilight/Ambilight.ino
+++ Arduino/Ambilight/Ambilight.ino
@@ -3,13 +3,15 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 13
-#define NUM_LEDS 30
-#define STRIP 30
+#define PIN 13 // Output pin on the Arduino
+#define NUM_LEDS 30 // The amount of LEDs used
+#define STRIP 30 // The amount of LEDs on the strip
+
+#define BAUD 115200 // This is the baud rate of which the program communicate with the Arduino
   
-char color[NUM_LEDS * 3 + 1];
-int placeAt = 0;
-int colorplace;
+char color[NUM_LEDS * 3 + 1]; // Make room for all the color data plus one byte for start
+int placeAt = 0; // This is used for the reading of color
+int colorplace; // This is used for the assignment of colors to the strip
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -26,40 +28,38 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIP, PIN, NEO_GRB + NEO_KHZ800);
 // on a live circuit...if you must, connect GND first.
 
 void setup() {
-  // put your setup code here, to run once:
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
   #endif
   
   strip.begin();
-  strip.setPixelColor(9, 1, 1, 1);//To see that it works
+  strip.setPixelColor(9, 1, 1, 1); // Just to see if the strip is working, this line can be removed
   strip.show();
   
-  Serial.begin(115200);
+  Serial.begin(BAUD);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available())
+  if(Serial.available()) // When something is sent over serial, this will be true
   {
-    color[placeAt] = Serial.read();
-    if(color[0] == 'a')
+    color[placeAt] = Serial.read(); // Read the serial into current placement space
+    if(color[0] == 'a') // Check whether first byte is our start byte
     {
-      placeAt++;
-      if(placeAt > NUM_LEDS * 3)
+      placeAt++; // Increment our placement position
+      if(placeAt > NUM_LEDS * 3) // Check if we have received colors for all our LEDs
       {
-        for(int i = 0; i < NUM_LEDS; i++)
+        for(int i = 0; i < NUM_LEDS; i++) // Go through each LED and set its color
         {
-          colorplace = i * 3 + 1;
-          strip.setPixelColor(i, color[colorplace], color[colorplace + 1], color[colorplace + 2]);
+          colorplace = i * 3 + 1; // Calculate the position of each value
+          strip.setPixelColor(i, color[colorplace], color[colorplace + 1], color[colorplace + 2]); // Set pixel(place, red, green, blue)
         }
-        strip.show();
-        placeAt = 0;
+        strip.show(); // Update the strip
+        placeAt = 0; // Set the placement to zero for next message to be received
       }
     }
     else
     {
-      placeAt = 0;
+      placeAt = 0; // Is our first byte is not our start byte we will reset the placement until we get the starting byte
     }
   }
 }
